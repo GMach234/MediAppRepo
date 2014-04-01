2eeab06aa4f34b46cc995694915573aaee4cdfb1
Michael Holmes <gmach234@gmail.com>
2014-04-01 01:13:26 +0100

    Adding sample Titanium projects

---
+++ FaveBooks/app/controllers/addbook.js
@@ -0,0 +1,19 @@
+var myBooks = Alloy.Collections.books;
+
+function addBook() {
+	var book = Alloy.createModel('books', {
+		title: $.titleInput.value,
+		author: $.authorInput.value
+	});
+	
+	myBooks.add(book);
+	book.save();
+	$.addbook.close();
+	
+	$.addbook.addEventListener("open", function() {
+	$titleInput.focus();
+	});
+	
+}
+
+
